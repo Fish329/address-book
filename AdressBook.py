e61376fb12fb543785df901c262f0748450c3550
Fish329 <268188@burlcoschools.org>
2025-02-07 09:22:25 -0500

    revert to better version

---
+++ AdressBook.py
@@ -0,0 +1,18 @@
+people = 11
+index=0
+names = ("Shahood Alvi", "Mason Bush", "Nathan Carn","Gavin Carnivale","Muntag Diallo","Leo Hart","Christopher Hodson","Marcos Paulino","Aidan Rodriguez","Ryan Stiles")
+ages = (17.00, 16.00, 17.00, 16.00, 17.10, 16.00, 17.00, 17.00, 16.73, 17.70, 17.33)
+haslicense = (False, False, False, True, False, True, False, False, False, True,)
+grade = (11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11)
+mail = ("267741@burlcoschools.org", "268262@burlcoschools.org","268021@burlcoschools.org","267764@burlcoschools.org","268154@burlcoschools.org","268095@burlcoschools.org","268359@burlcoschools.org","267895@burlcoschools.org","267922@burlcoschools.org","267947@burlcoschools.org")
+while index<people-1:
+ print("-----------------------ID CARD------------------------")
+ print("NAME:",names[index])
+ print("AGE:",ages[index])
+ print("DRIVERS lICENSE?",haslicense[index])
+ print("GRADE:",grade[index])
+ print("EMAIL:",mail[index])
+ print("--------------------------BCIT------------------------")
+ print("")
+ index += 1
+print("scroll through the terminal to see each ID card!")
